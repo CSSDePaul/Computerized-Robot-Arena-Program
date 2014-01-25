94b60d3453f2ec3594271771ca3ff491c5ea09ee
Jordan Myers <jordan.v.myers@gmail.com>
2014-01-24 22:39:14 -0600

    implemented turning

--- boardState.py
+++ boardState.py
@@ -2,16 +2,14 @@ from robot import robot
 
 class boardState:
 	BOARD_SIZE = 10
-	MOVES = ['turnleft', 'turnright', 'forward']
-	MOVEVECTORS = {'turnleft':	 (0, 0, -90), #(x, y, t)
-				'turnright': (0, 0, 90),
-				'forward':   (0, 1, 0),
+	MOVEFUNCTIONS = {'turnLeft':	 robot.turnLeft,
+				'turnRight': robot.turnRight,
+				'moveForward':   robot.moveForward,
 				}
-	Players = [None, None]
+	Players = []
 	
 	def __init__(self):
-		self.Players[0] = robot(0,0,0, "Robot0")
-		self.Players[1] = robot(1,1,0, "Robot1")
+		self.Players.append(robot(0,0,0, "Robot0"))
 	
 	def update(self):
 		for player in self.Players:
--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+import random
+from math import *
 
 class robot:
 	xPosition = 0
@@ -11,26 +13,34 @@ class robot:
 		self.thetaPosition = t
 		self.name = Str
 		
-	def takeAction(self, boardState):
-		move = 'forward'
-		xVector = boardState.MOVEVECTORS[move][0]
-		yVector = boardState.MOVEVECTORS[move][1]
-		tVector = boardState.MOVEVECTORS[move][2]
-		newX = self.xPosition + xVector
-		newY = self.yPosition + yVector
-		newT = self.thetaPosition + tVector
-		if self.isValidMove(newX, newY, newT, boardState):
-			self.xPosition = newX
-			self.yPosition = newY
-			self.thetaPosition = newT
+	def decideAction(self, boardState): #this is what is scripted by user
+		return random.choice(list(boardState.MOVEFUNCTIONS.keys()))
 		
+	def takeAction(self, boardState):
+		global theFunction
+		action = self.decideAction(boardState)
+		print("action taken by %s is %s"%(self.name, action))
+		theFunction = boardState.MOVEFUNCTIONS[action]
+		theFunction(self, boardState)	
+		
+	def theFunction(self, boardState): #is replaced when takeAction is called
+		raise NotImplementedError
+
+	def turnLeft(self, boardState):
+		self.thetaPosition -= 90
 	
-	def isValidMove(self, x, y, t, boardState):
-		if 	x < 0 or x >= boardState.BOARD_SIZE:
-			return False
-		if y < 0 or y >= boardState.BOARD_SIZE:
-			return False
-		return True		
+	def turnRight(self, boardState):
+		self.thetaPosition += 90
+
+	def moveForward(self, boardState):
+		newX = self.xPosition + sin(radians(self.thetaPosition))
+		newY = self.yPosition + cos(radians(self.thetaPosition))
+		if newX < 0 or newX >= boardState.BOARD_SIZE:
+			return
+		if newY < 0 or newY >= boardState.BOARD_SIZE:
+			return
+		self.xPosition = newX
+		self.yPosition = newY
 
 	def __str__(self):
 		return "Robot %s at (%i, %i) facing %i degrees" %(\
