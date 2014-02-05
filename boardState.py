4c482c3fa847c6d379e1c2e230c3a4779c1db8d7
excaliburhissheath <excaliburhissheath@ehs-Homefront>
2014-02-04 19:42:44 -0600

    Formats comments as proper docstrings.

--- boardState.py
+++ boardState.py
@@ -2,18 +2,39 @@ from robot import robot
 
 class boardState:
 	BOARD_SIZE = 10
-	MOVEFUNCTIONS = {	'turnLeft':	 	robot.turnLeft,
-						'turnRight': 	robot.turnRight,
-						'moveForward':	robot.moveForward,
-					}
-	Players = []  # the list of robot objects
+	'''
+	The size of the board, specifically the number of tiles on the side of a square board.
 	
-	# the constructor. Initializes robots to add to boardState
+	If BOARD_SIZE is 0, the board is unbounded.
+	'''
+	
+	MOVEFUNCTIONS = {
+					'turnLeft':	 	robot.turnLeft,
+					'turnRight': 	robot.turnRight,
+					'moveForward':	robot.moveForward,
+					}
+	'''
+	A dict containing the available functions for robots to perform
+	and their corresponding function calls.
+	'''
+	
+	robots = []
+	'''
+	A list containing the robots on the board.
+	'''
+	
 	def __init__(self):
-		self.Players.append(robot(0, 0, 0, "Robot0"))
+		'''
+		Initializes robots to add to boardState
+		'''
+		
+		self.robots.append(robot(0, 0, 0, "Robot0"))
 
-	# updates positions of all objects
 	def update(self):
-		for player in self.Players:
+		'''
+		Updates the positions of all robots and performs all other simulation logic.
+		'''
+		
+		for player in self.robots:
 			player.takeAction(self)
 			print(player)
