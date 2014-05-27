4a55bc7aa5d69d6498d88955a76286f2691d79f8
David LeGare <excaliburhissheath@gmail.com>
2014-05-26 21:55:48 -0500

    removes useless debug spew.

--- example_scripts/directWalk.py
+++ example_scripts/directWalk.py
@@ -19,46 +19,44 @@ class robotBehavior:
         '''
         Walks towards the other robot and tries to run into it.
         '''
-        
+
         # get the list of robots from the board
         robots = board.getRobots()
-        
+
         # if no other robot, do nothing
         if len(robots) <= 1:
             return
-        
+
         robotKeys = list(robots.keys())
-        
+
         # temporary hacky workaround
         # get the other robot (future versions should find the closest robot)
         closest = robots[robotKeys[0]]
-        
+
         # if first robot is this robot, get second robot
         if robotKeys[0] == robot.name:
             closest = robots[robotKeys[1]]
-        
+
         # get current distance to target robot
         currentDistance = manhattanDistance(robot, closest)
-        
+
         # get coordinates of space in front of robot
         forwardPos = forwardCoords(robot.xPosition, robot.yPosition, robot.rotation)
-        
+
         # if other robot is right in front, shoot that robot
         actors_ahead = board.occupied(forwardPos[0], forwardPos[1])
-        
-        logging.debug(actors_ahead)
-        
+
         fire = False
         for actor_name in actors_ahead:
             fire = True if isinstance(board.actors[actor_name], Robot) else fire
-            
+
         if fire:
             logging.debug('FIRE AWAY')
             return 'SHOOT_PROJECTILE'
-        
+
         # get distance of forwardPos from target
         forwardDistance = manhattanDistance(forwardPos, closest)
-        
+
         # if forward gets robot closer to target, move forward
         if forwardDistance < currentDistance:
             return 'MOVE_FORWARD'
@@ -69,4 +67,3 @@ class robotBehavior:
     def __init__(self):
 
         pass
-        
